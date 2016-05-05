0cd2817c0ceb45defd1dd93d51f35eaa49bb7e0b
Matej Grozdanović <grozdanowski@gmail.com>
2016-05-05 11:03:27 +0200

    initial commit of new codebase

--- static/js/base.js
+++ static/js/base.js
@@ -1,20 +1,55 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+$(document).ready(function(){
 
+	// site mobile nav toggle
+	$('#site-menu-toggle').on('click', function() {
+		$(this).toggleClass('a--toggled');
+		$('#slide-menu').toggleClass('a--active');
+	});
 
-//
+	// execute css on-load animations
+	$('.a--animated').addClass("a--do-animate");
 
+	function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
 
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
 
-$("#dashboard-header__search-trigger").click(function() {
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+	$(window).scroll(function () {
+	   $('.a--animate-on-scroll.a--out-animation').each(function () {
+	      if (isScrolledIntoView(this) === true) {
+	          $(this).addClass('a--in-view');
+	      } else {
+					$(this).removeClass('a--in-view');
+				}
+	   });
+	});
+
+	$(window).scroll(function () {
+	   $('.a--animate-on-scroll.a--no-out-animation').each(function () {
+	      if (isScrolledIntoView(this) === true) {
+	          $(this).addClass('a--in-view');
+	      }
+	   });
+	});
+
+	$('.a--reveal-on-load').each(function() {
+		$(this).addClass('a--revealed');
+	});
+
+	$('.a--expanding-section').on('click', function() {
+	 $('.a--expanding-section').each((function(index, el) {
+		 $(this).removeClass('a--expanded');
+			 }));
+	 $(this).toggleClass('a--expanded');
+ 	});
+
+	$("#discovery-expand-button").click(function() {
     $(this).toggleClass("activated");
-    $("#dashboard-search-bar").slideToggle();
-})
+    $("#discovery-expanding-area").slideToggle();
+	})
+});
